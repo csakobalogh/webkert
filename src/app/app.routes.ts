3a0e7e234199db824b4e9d2f23067f2e332c2ff2
csakobalogh <48767066+csakobalogh@users.noreply.github.com>
2025-04-03 16:12:28 +0200

    working menu without any styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
 import { ProductsComponent } from './pages/products/products.component';
 import { ProfileComponent } from './pages/profile/profile.component';
-import { SignupComponent } from './pages/signup/signup.component';
 import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
@@ -12,7 +12,7 @@ export const routes: Routes = [
     {path: 'login', component: LoginComponent},
     {path: 'products', component: ProductsComponent},
     {path: 'profile', component: ProfileComponent},
-    {path: 'signup', component: SignupComponent},
+    {path: 'registration', component: RegistrationComponent},
 
     // Parameterezett utvonalak
     // ...
