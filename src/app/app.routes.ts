f20369689ca6edfb92ae34fdd7a1472055f701f6
csakobalogh <48767066+csakobalogh@users.noreply.github.com>
2025-04-03 17:36:09 +0200

    updated routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,20 +7,33 @@ import { ProfileComponent } from './pages/profile/profile.component';
 import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-    // Statikus eleresi utvonalak
-    {path: 'home', component: HomeComponent},
-    {path: 'login', component: LoginComponent},
-    {path: 'products', component: ProductsComponent},
-    {path: 'profile', component: ProfileComponent},
-    {path: 'registration', component: RegistrationComponent},
-
-    // Parameterezett utvonalak
-    // ...
-
-    // Ures eleresi utvonal
-    {path: '', redirectTo: 'home', pathMatch: 'full'},
-    // {path: '**', component: HomeComponent}
-
-    // Wildcard utvonal
-    {path: '**', component: PageNotFoundComponent}
+    {
+        path: 'home',
+        loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
+    },
+    {
+        path: 'products',
+        loadComponent: () => import('./pages/products/products.component').then(m => m.ProductsComponent)
+    },
+    {
+        path: 'profile',
+        loadComponent: () => import('./pages/profile/profile.component').then(m => m.ProfileComponent)
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent)
+    },
+    {
+        path: 'registration',
+        loadComponent: () => import('./pages/registration/registration.component').then(m => m.RegistrationComponent)
+    },
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        loadComponent: () => import('./shared/page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent)
+    }
 ];
