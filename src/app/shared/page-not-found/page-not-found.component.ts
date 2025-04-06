7e92de2d7ed7774783acaf63b6a3440b48f93447
csakobalogh <48767066+csakobalogh@users.noreply.github.com>
2025-04-06 14:50:28 +0200

    modified page-not-found component to look better

--- src/app/shared/page-not-found/page-not-found.component.ts
+++ src/app/shared/page-not-found/page-not-found.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterLink, /* RouterLinkActive */ } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-page-not-found',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [
+    RouterLink, 
+    // RouterLinkActive, 
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.scss'
 })
