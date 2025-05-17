54a06e1b0ab1f6636ffe2858da964e037d4e6674
csakobalogh <48767066+csakobalogh@users.noreply.github.com>
2025-05-17 09:45:15 +0200

    added another query

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,10 +3,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { Router, RouterLink } from '@angular/router';
 import { ProductService } from '../../shared/services/product.service';
 import { Product } from '../../shared/models/Product';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [MatButtonModule, RouterLink],
+  imports: [MatButtonModule, FormsModule, RouterLink],
+  standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -14,6 +16,10 @@ export class HomeComponent implements OnInit {
   isLoggedIn = false;
   cheapRugs: Product[] = [];
   topRatedRugs: Product[] = [];
+  colorFilteredRugs: Product[] = [];
+
+  availableColors: string[] = ['piros', 'zöld', 'kék', 'barna', 'szürke'];
+  selectedColor: string = '';
 
   constructor(private router: Router, private productService: ProductService) {}
 
@@ -21,6 +27,16 @@ export class HomeComponent implements OnInit {
     this.isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
     this.loadCheapRugs();
     this.loadTopRatedRugs();
+  }
+
+  onColorChange(): void {
+    if (!this.selectedColor) {
+      this.colorFilteredRugs = [];
+      return;
+    }
+    
+    this.productService.getProductsByColorSortedByPrice(this.selectedColor)
+      .subscribe(rugs => this.colorFilteredRugs = rugs);
   }
 
   loadCheapRugs(): void {
